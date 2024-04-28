45b4cb9016bf54af88fd74445005488688657ed0
MaluMSiza <93840950+MaluMSiza@users.noreply.github.com>
2024-04-28 18:09:07 -0300

    Delete Servidor directory

--- Servidor/src/config/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/produtos';
-mongoose.connect(url, { useNewUrlParser: true }); // https://mongoosejs.com/docs/5.x/docs/deprecations.html
-
-module.exports = mongoose;
--- Servidor/src/routes/CidadeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CidadeController = require('../controller/CidadeController');
-
-router.get('/:id',  CidadeController.get);
-router.get('/filter/getAll',  CidadeController.getAll);
-
-module.exports = router;
